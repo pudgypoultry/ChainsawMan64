bf25411c6aff25788b8c68783155ca7c42466b53
pudgypoultry <50252390+pudgypoultry@users.noreply.github.com>
2023-05-17 15:27:43 -0700

    Initial commit

---
+++ Assets/Scripts/DenjiController.cs
@@ -0,0 +1,182 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DenjiController : MonoBehaviour
+{
+
+    Animator animatorController;
+    float velocityZ = 0.0f;
+    float velocityX = 0.0f;
+    public float acceleration = 2.0f;
+    public float deceleration = 2.0f;
+    public float maxWalkVelocity = 0.5f;
+    public float maxRunVelocity = 2.0f;
+
+    int isWalkingHash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animatorController = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("isWalking");
+        Debug.Log(animatorController);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // input will be true if the player is pressing on the passed in key parameter
+        // get key input from player
+        bool forwardPressed = Input.GetKey("w");
+        bool leftPressed = Input.GetKey("a");
+        bool rightPressed = Input.GetKey("d");
+        bool runPressed = Input.GetKey("left shift");
+        bool attackPressed = Input.GetKey("e");
+        Debug.Log("I'M HERE");
+        // set current maxVelocity
+        float currentMaxVelocity = runPressed ? maxRunVelocity : maxWalkVelocity;
+
+        changeVelocity(forwardPressed, leftPressed, rightPressed, runPressed, currentMaxVelocity);
+
+        lockOrResetVelocity(forwardPressed, leftPressed, rightPressed, runPressed, currentMaxVelocity);
+
+        handleAttacking(attackPressed);
+
+        animatorController.SetFloat("VelocityZ", velocityZ);
+        animatorController.SetFloat("VelocityX", velocityX);
+
+    }
+
+    void changeVelocity(bool forwardPressed, bool leftPressed, bool rightPressed, bool runPressed, float currentMaxVelocity)
+    {
+        if (forwardPressed && velocityZ < currentMaxVelocity)
+        {
+            velocityZ += Time.deltaTime * acceleration;
+        }
+
+        if (!forwardPressed && velocityZ > 0.0f)
+        {
+            velocityZ -= Time.deltaTime * deceleration;
+        }
+
+        if (leftPressed && velocityX > -currentMaxVelocity)
+        {
+            velocityX -= Time.deltaTime * acceleration;
+        }
+
+        if (rightPressed && velocityX < currentMaxVelocity)
+        {
+            velocityX += Time.deltaTime * acceleration;
+        }
+
+        // reset velocityZ
+        if (!forwardPressed && velocityZ < 0.0f)
+        {
+            velocityZ = 0.0f;
+        }
+
+        // reset velocityX
+        if (!leftPressed && velocityX < 0.0f)
+        {
+            velocityX += Time.deltaTime * deceleration;
+        }
+
+        if (!rightPressed && velocityX > 0.0f)
+        {
+            velocityX -= Time.deltaTime * deceleration;
+        }
+
+        if (!leftPressed && !rightPressed && velocityX != 0.0f && (velocityX > -0.0f && velocityX < 0.05f))
+        {
+            velocityX = 0.0f;
+        }
+    }
+
+    void lockOrResetVelocity(bool forwardPressed, bool leftPressed, bool rightPressed, bool runPressed, float currentMaxVelocity)
+    {
+        // lock forward
+        if (forwardPressed && runPressed && velocityZ > currentMaxVelocity)
+        {
+            velocityZ = currentMaxVelocity;
+        }
+        // decelerate to the maximum walk velocity
+        else if (forwardPressed && velocityZ > currentMaxVelocity)
+        {
+            velocityZ -= Time.deltaTime * deceleration;
+
+            if (velocityZ > currentMaxVelocity && velocityZ < (currentMaxVelocity + 0.05f))
+            {
+                velocityZ = currentMaxVelocity;
+            }
+        }
+        // round to the currentMaxVelocity if within offset
+        else if (forwardPressed && velocityZ < currentMaxVelocity && velocityZ > (currentMaxVelocity - 0.05f))
+        {
+            velocityZ = currentMaxVelocity;
+        }
+
+        // lock right
+        if (rightPressed && runPressed && velocityX > currentMaxVelocity)
+        {
+            velocityX = currentMaxVelocity;
+        }
+        // decelerate to the maximum walk velocity
+        else if (rightPressed && velocityX > currentMaxVelocity)
+        {
+            velocityX -= Time.deltaTime * deceleration;
+
+            if (velocityX > currentMaxVelocity && velocityX < (currentMaxVelocity + 0.05f))
+            {
+                velocityX = currentMaxVelocity;
+            }
+        }
+        // round to the currentMaxVelocity if within offset
+        else if (rightPressed && velocityX < currentMaxVelocity && velocityX > (currentMaxVelocity - 0.05f))
+        {
+            velocityX = currentMaxVelocity;
+        }
+
+        // lock left
+        if (leftPressed && runPressed && velocityX < -currentMaxVelocity)
+        {
+            velocityX = -currentMaxVelocity;
+        }
+        // decelerate to the maximum walk velocity
+        else if (leftPressed && velocityX < -currentMaxVelocity)
+        {
+            velocityX += Time.deltaTime * deceleration;
+
+            if (velocityX < -currentMaxVelocity && velocityX > (-currentMaxVelocity - 0.05f))
+            {
+                velocityX = -currentMaxVelocity;
+            }
+        }
+        // round to the currentMaxVelocity if within offset
+        else if (leftPressed && velocityX > -currentMaxVelocity && velocityX < (-currentMaxVelocity + 0.05f))
+        {
+            velocityX = -currentMaxVelocity;
+        }
+    }
+
+    void handleAttacking(bool attackPressed)
+    {
+        animatorController.SetBool("isBasicAttacking", attackPressed);
+    }
+
+    /*
+
+        bool isWalking = animatorController.GetBool(isWalkingHash);
+        bool forwardPressed = Input.GetKey("w");
+        if (!isWalking && forwardPressed)
+        {
+            animatorController.SetBool(isWalkingHash, true);
+        }
+
+        if (isWalking && !forwardPressed)
+        {
+            animatorController.SetBool(isWalkingHash, false);
+        }
+
+    */
+}
